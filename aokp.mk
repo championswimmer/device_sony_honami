99f7f8158ab451fe724595ca70b5af81bc4b1a82
Arnav Gupta <championswimmer@aokp.co>
2013-12-25 03:08:42 +0530

    honami: update fingerprint
    Change-Id: Icef808fbd872aa613aaa00b27edb1cd56bd91702
    Signed-off-by: Arnav Gupta <championswimmer@aokp.co>

--- aokp.mk
+++ aokp.mk
@@ -7,6 +7,6 @@ $(call inherit-product, vendor/aokp/configs/gsm.mk)
 # Inherit AOSP device configuration for honami
 $(call inherit-product, device/sony/honami/full_honami.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903 BUILD_FINGERPRINT=Sony/C6903_1276-7948/C6903:4.2.2/14.1.G.1.526/8Xl-jw:user/release-keys PRIVATE_BUILD_DESC="C6903-user 4.2.2 14.1.G.1.526 8Xl-jw test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903 BUILD_FINGERPRINT=Sony/C6903/C6903:4.3/14.2.A.0.290/eng.hudsonslave:user/release-keys PRIVATE_BUILD_DESC="C6903-user 4.3 RHINE-1.1-131125-1201 eng.hudsonslave test-keys"
 
 TARGET_OTA_ASSERT_SKIP=true
